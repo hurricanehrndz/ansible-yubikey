7a76404d61cfc63c274700e84b3d3f3c6d7a4f61
Carlos Hernandez <carlos@techbyte.ca>
2020-03-01 07:27:10 -0700

    Verify functionality with testinfra

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,3 +12,21 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def test_ssh(host):
+    command = f"timeout 5 pamtester sshd hurricane authenticate"
+    cmd = host.run(command)
+    assert (f"YubiKey" in cmd.stderr)
+
+
+def test_login(host):
+    command = f"timeout 5 pamtester login hurricane authenticate"
+    cmd = host.run(command)
+    assert (f"YubiKey" in cmd.stderr)
+
+
+def test_sudo(host):
+    command = f"timeout 5 pamtester sudo hurricane authenticate"
+    cmd = host.run(command)
+    assert (f"YubiKey" in cmd.stderr)
